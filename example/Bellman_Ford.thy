c015d3d45f9f0235e1001a1a007bbec855d7e45e
exprosic <exprosic@gmail.com>
2017-06-22 18:11:25 +0200

    minor fix

--- example/Bellman_Ford.thy
+++ example/Bellman_Ford.thy
@@ -178,6 +178,6 @@ lemma "bf.consistentDP bf\<^sub>T"
                         apply transfer_step
                         apply transfer_step (*?Rp38 becomes (K j), but it should be (op =)*)
                         apply transfer_step
-
+      oops
 end
 end
