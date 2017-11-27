6f88d9a96ab9572df98bbd4d1e9d413020440bae
Simon Wimmer <wimmers@in.tum.de>
2017-11-27 10:54:13 +0100

    More streamlined proof

--- example/Knapsack.thy
+++ example/Knapsack.thy
@@ -28,85 +28,81 @@ definition
 
 lemma OPT_0:
   "OPT 0 W = 0"
-  unfolding OPT_def by auto
+  unfolding OPT_def by simp
+
+lemma Max_add_left:
+  "(x :: nat) + Max S = Max ((op + x) ` S)" (is "?A = ?B") if "finite S" "S \<noteq> {}"
+proof -
+  have "?A \<le> ?B"
+    using that by (force intro: Min.boundedI)
+  moreover have "?B \<le> ?A"
+    using that by (force intro: Min.boundedI)
+  ultimately show ?thesis
+    by simp
+qed
 
 lemma OPT_Suc:
   "OPT (Suc i) W = (
     if W < w (Suc i)
     then OPT i W
     else max(v (Suc i) + OPT i (W - w (Suc i))) (OPT i W)
-  )"
+  )" (is "?lhs = ?rhs")
 proof -
   have OPT_in: "OPT n W \<in> {\<Sum> i \<in> S. v i | S. S \<subseteq> {1..n} \<and> (\<Sum> i \<in> S. w i) \<le> W}" for n W
     unfolding OPT_def by - (rule Max_in; force)
   from OPT_in[of "Suc i" W] obtain S where S:
     "S \<subseteq> {1..Suc i}" "sum w S \<le> W" and [simp]: "OPT (Suc i) W = sum v S"
     by auto
+
   have "OPT i W \<le> OPT (Suc i) W"
     unfolding OPT_def by (force intro: Max_mono)
-  have "sum v S = OPT i W" if "W < w (Suc i)"
+  moreover have "v (Suc i) + OPT i (W - w (Suc i)) \<le> OPT (Suc i) W" if "w (Suc i) \<le> W"
   proof -
-    have "Suc i \<notin> S"
-    proof (rule ccontr, simp)
-      assume "Suc i \<in> S"
-      then have "sum w S \<ge> w (Suc i)"
-        using S(1) by (subst sum.remove) (auto intro: finite_subset[OF _ finite_atLeastAtMost])
-      with \<open>W < _\<close> \<open>_ \<le> W\<close> show False
-        by simp
-    qed
-    with S have "OPT i W \<ge> sum v S"
-      unfolding OPT_def by (auto simp: atLeastAtMostSuc_conv intro!: Max_ge)
-    with \<open>OPT i W \<le> _\<close> show ?thesis
+    have *: "
+      v (Suc i) + sum v S = sum v (S \<union> {Suc i}) \<and> (S \<union> {Suc i}) \<subseteq> {1..Suc i}
+      \<and> sum w (S \<union> {Suc i}) \<le> W" if "S \<subseteq> {1..i}" "sum w S \<le> W - w (Suc i)" for S
+      using that \<open>w (Suc i) \<le> W\<close>
+      by (subst sum.insert_if | auto intro: finite_subset[OF _ finite_atLeastAtMost])+
+    show ?thesis
+      unfolding OPT_def
+      by (subst Max_add_left;
+          fastforce intro: Max_mono finite_subset[OF _ finite_atLeastAtMost] dest: *
+         )
+  qed
+  ultimately have "?lhs \<ge> ?rhs"
+    by auto
+
+  from S have *: "sum v S \<le> OPT i W" if "Suc i \<notin> S"
+    using that unfolding OPT_def by (auto simp: atLeastAtMostSuc_conv intro!: Max_ge)
+
+  have "sum v S \<le> OPT i W" if "W < w (Suc i)"
+  proof (rule *, rule ccontr, simp)
+    assume "Suc i \<in> S"
+    then have "sum w S \<ge> w (Suc i)"
+      using S(1) by (subst sum.remove) (auto intro: finite_subset[OF _ finite_atLeastAtMost])
+    with \<open>W < _\<close> \<open>_ \<le> W\<close> show False
       by simp
   qed
   moreover have
-    "OPT (Suc i) W = max(v (Suc i) + OPT i (W - w (Suc i))) (OPT i W)" if "w (Suc i) \<le> W"
+    "OPT (Suc i) W \<le> max(v (Suc i) + OPT i (W - w (Suc i))) (OPT i W)" if "w (Suc i) \<le> W"
   proof (cases "Suc i \<in> S")
     case True
     then have [simp]:
       "sum v S = v (Suc i) + sum v (S - {Suc i})" "sum w S = w (Suc i) + sum w (S - {Suc i})"
       using S(1) by (auto intro: finite_subset[OF _ finite_atLeastAtMost] sum.remove)
-    have "sum v S' \<le> sum v (S - {Suc i})" if "S' \<subseteq> {Suc 0..i}" "sum w S' \<le> W - w (Suc i)" for S'
-    proof -
-      have "sum v S' + v (Suc i) \<in> {\<Sum> i \<in> S. v i | S. S \<subseteq> {1..Suc i} \<and> (\<Sum> i \<in> S. w i) \<le> W}"
-        using that \<open>w (Suc i) \<le> W\<close>
-        apply simp
-        apply (rule exI[where x = "S' \<union> {Suc i}"])
-        apply clarsimp
-        by ((subst sum.insert_if, auto intro: finite_subset[OF _ finite_atLeastAtMost]))+
-      then have "sum v S' + v (Suc i) \<le> OPT (Suc i) W"
-        unfolding OPT_def by auto
-      then show ?thesis
-        by simp
-    qed
-    then have "OPT i (W - w (Suc i)) = sum v (S - {Suc i})"
-      unfolding OPT_def using S by (fastforce intro!: Max_eqI)
-    with \<open>OPT i W \<le> _\<close> show ?thesis
+    have "OPT i (W - w (Suc i)) \<ge> sum v (S - {Suc i})"
+      unfolding OPT_def using S by (fastforce intro!: Max_ge)
+    then show ?thesis
       by simp
   next
     case False
-    with S have "OPT (Suc i) W \<le> OPT i W"
-      by (simp, auto simp: atLeastAtMostSuc_conv OPT_def intro!: Max_ge)
-    with \<open>OPT i W \<le> _\<close> have "OPT (Suc i) W = OPT i W"
-      by simp
-    moreover have "v (Suc i) + local.OPT i (W - w (Suc i)) \<le> OPT (Suc i) W"
-    proof -
-      from OPT_in[of i "W - w (Suc i)"] guess S'
-        by clarify
-      then show ?thesis
-        unfolding OPT_def
-        apply -
-        apply (rule Max_ge)
-         apply force
-        apply (rule CollectI, rule exI[where x = "S' \<union> {Suc i}"])
-        using \<open>w (Suc i) \<le> W\<close>
-        by simp ((subst sum.insert_if; auto intro: finite_subset[OF _ finite_atLeastAtMost]))+
-    qed
-    with \<open>_ = OPT i W\<close> show ?thesis
-      by simp
+    then show ?thesis
+      by (auto dest: *)
   qed
-  ultimately show ?thesis
+  ultimately have "?lhs \<le> ?rhs"
     by auto
+  with \<open>?lhs \<ge> ?rhs\<close> show ?thesis
+    by simp
 qed
 
 theorem knapsack_correct:
